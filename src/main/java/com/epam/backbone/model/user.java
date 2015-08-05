9b0c15b65dec01d17a14513bbd2c2b9ffd2f3801
Rody <Rody10@mail.ru>
2015-08-06 00:14:46 +0300

    Prepared app

--- src/main/java/com/epam/backbone/model/user.java
+++ src/main/java/com/epam/backbone/model/user.java
@@ -1,7 +1,39 @@
 package com.epam.backbone.model;
 
-/**
- * Created by Rody on 05.08.2015.
- */
+
 public class user {
+
+    private String firstname;
+    private String lasttname;
+    private int age;
+
+    public user(String firstname, String lasttname, int age) {
+        this.firstname = firstname;
+        this.lasttname = lasttname;
+        this.age = age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLasttname() {
+        return lasttname;
+    }
+
+    public void setLasttname(String lasttname) {
+        this.lasttname = lasttname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/main/java/com/epam/backbone/rest/userService.java
+++ src/main/java/com/epam/backbone/rest/userService.java
@@ -1,7 +1,28 @@
 package com.epam.backbone.rest;
 
-/**
- * Created by Rody on 05.08.2015.
- */
+
+import com.epam.backbone.model.user;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users/")
 public class userService {
+
+    @RequestMapping( value = "/get-all/", method = RequestMethod.GET)
+    public @ResponseBody List<user> getUsersList(){
+        ArrayList<user> users = new ArrayList<user>();
+
+        users.add(new user("Mark","Johnson", 12));
+        users.add(new user("Scott","Donovan", 12));
+        users.add(new user("Jerry","Zukerman", 12));
+        users.add(new user("Bony","Fletcher", 12));
+
+        return users;
+    }
 }
